8b42af9db4d9f1507b703a776aa469eb0ea95f4b
rafabrutaldrums <handlygore@hotmail.com>
2021-01-01 22:47:40 -0600

    deployment

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ connectDB();
 app.use(express.json({ extended: true }))
 app.use(cors({origin: 'http://localhost:3000'}));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.port || 4000;
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-app.listen(PORT , ()=>{
+app.listen(port, '0.0.0.0', ()=>{
    console.log(`Running server in port ${PORT}`) 
 })
